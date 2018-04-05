fb2bd94facc05ffc59119b615e226769f2203ead
Matthew Christensen <matt.christensen@byu.edu>
2018-04-04 22:17:21 -0600

    add input form

--- homepage/views.py
+++ homepage/views.py
@@ -12,6 +12,31 @@ from django.template.loader import render_to_string, get_template
 # Create your views here.
 def landingpage(request):
 
-    context = {}
+    if request.method == 'POST':
+        form = VideoGameForm(request.POST, request=request)
+        if form.is_valid():
+            form.commit()
+
+            return HttpResponseRedirect('/homepage/landingpage/')
+    else:
+        form = VideoGameForm(request=request)
+    context = {
+        'form': form,
+    }
 
     return render(request, 'homepage/landingpage.html', context)
+
+class VideoGameForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(VideoGameForm, self).__init__(*args, **kwargs)
+
+        self.fields['first_name'] = forms.CharField(label="First Name", required=True, max_length=100, widget=forms.TextInput(attrs={'placeholder':'First Name', 'class':'form-control'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        pass
+
+    def commit(self):
+        pass
